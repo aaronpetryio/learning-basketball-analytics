cce58070b16b8c846d6e367049c4268ac6499fa2
Aaron Petry <aaron@aaronpetry.io>
2021-11-27 22:25:40 -0800

    added multi-year funcitonality and year column

--- scraper/bref-scraper.py
+++ scraper/bref-scraper.py
@@ -23,28 +23,39 @@ url = "https://www.basketball-reference.com/leagues/NBA_" + years[0] + ".html#mi
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
 
-#Find the advanced stats header row
+#Find the advanced stats header row (skip the top header row and ignore rank)
 adv_stats_table = soup.find('table', attrs={'id':'advanced-team'})
 adv_stats_headers = adv_stats_table.find('thead')
 headers = adv_stats_headers.find_all('th')
-headers = [ele.text.strip() for ele in headers]
+headers = [ele.text.strip() for ele in headers[6:]]
+
+#Manually add the 'Year' header
+year_header = ['Year']
+combined_headers = year_header + headers
 
 #Append the headers to the beginning of the nba_adv_stats list
-nba_adv_stats.append([ele for ele in headers])
+nba_adv_stats.append([ele for ele in combined_headers])
 
 #Find the advanced stats body 
 adv_stats_body = adv_stats_table.find('tbody')
 adv_stats_rows = adv_stats_body.find_all('tr')
 
-#For each row in the advanced stats table, add the team data to the nba_adv_stats list
-for row in adv_stats_rows:
+#For each year in the years list, add the advanced stats data
+for year in years:
+    #For each row in the advanced stats table, add the team data to the nba_adv_stats list
+    for row in adv_stats_rows:
+        #Populate the Year column (will need to update once this is a loop)
+        year_column = [year]
 
-    #Find the rest of the data columns
-    data_columns = row.find_all('td')
-    data_columns = [ele.text.strip() for ele in data_columns]
-    
-    #Combine the columns and add them to the nba_adv_list list
-    nba_adv_stats.append([ele for ele in data_columns])
+        #Find the rest of the data columns
+        data_columns = row.find_all('td')
+        data_columns = [ele.text.strip() for ele in data_columns]
+
+        #Combine columns
+        combined_columns = year_column + data_columns
+        
+        #Combine the columns and add them to the nba_adv_list list
+        nba_adv_stats.append([ele for ele in combined_columns])
 
 #Create a dynamic CSV file name based on the year scraped
 csv_path = os.path.join(os.getcwd(), "data/")
